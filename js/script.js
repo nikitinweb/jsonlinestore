94af9146ba640afe9f08eb1d1edadc74bd87b281
Alexander Nikitin <anikitin.web@gmail.com>
2019-02-19 21:04:55 +0300

    Add task 2

--- js/script.js
+++ js/script.js
@@ -36,14 +36,81 @@ window.addEventListener('DOMContentLoaded', () => {
 			removeBtn.classList.add('goods__item-remove');
 			removeBtn.innerHTML = '&times';
 
-			item.appendChild(removeBtn);
+			showConfirm();
 
+			item.appendChild(removeBtn);
 			cartWrapper.appendChild(item);
 
+			calcGoods();
+
 			if (empty) {
-				empty.remove();
+				empty.style.display = 'none';
 			}
+
+			calcTotal();
+			removeFromCart();
 		});
 	});
 
+	function sliceTitle() {
+		titles.forEach(function (item) {
+			if (item.textContent.lenght < 70) {
+				return;
+			} else {
+				const str = item.textContent.slice(0, 71) + '...';
+				item.textContent = str;
+			}
+		});
+	}
+	sliceTitle();
+
+	function showConfirm() {
+		confirm.style.display = 'block';
+
+		let counter = 100;
+
+		const id = setInterval(frame, 10);
+
+		function frame() {
+			if (counter == 10) {
+				clearInterval(id);
+				confirm.style.display = 'none';
+			} else {
+				counter--;
+				confirm.style.transform = `translateY(-${counter}px)`;
+				confirm.style.opacity = '0.' + counter;
+			}
+		}
+	}
+
+	function calcGoods() {
+		const items = cartWrapper.querySelectorAll('.goods__item');
+		badge.textContent = items.length;
+
+		let empty = cartWrapper.querySelector('.empty');
+
+		if (items.length == 0) empty.style.display = 'block';
+	}
+
+	function calcTotal() {
+		const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+
+		let total = 0;
+		prices.forEach(function (item) {
+			total += +item.textContent;
+		})
+
+		totalCost.textContent = total;
+	}
+
+	function removeFromCart() {
+		const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+		removeBtn.forEach(function (btn) {
+			btn.addEventListener('click', () => {
+				btn.parentElement.remove();
+				calcGoods();
+				calcTotal();
+			});
+		});
+	}
 });
